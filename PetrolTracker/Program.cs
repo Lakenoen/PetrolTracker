1d62ba3c98b8b0818c09b5e51069c1837dfa3dad
Lakenoen <lakenoen@gmail.com>
2026-05-01 14:59:34 +0500

    added filters

--- PetrolTracker/Program.cs
+++ PetrolTracker/Program.cs
@@ -1,9 +1,18 @@
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+using Microsoft.AspNetCore.Http.Json;
 using DbManager;
 
 GlobalSettings.UpdateDB = false;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
+builder.Services.Configure<JsonOptions>(options =>
+{
+    options.SerializerOptions.Encoder = JavaScriptEncoder.Create(UnicodeRanges.All);
+    options.SerializerOptions.PropertyNameCaseInsensitive = true;
+});
+
 var app = builder.Build();
 
 app.MapRazorPages();
