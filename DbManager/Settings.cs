84f03fc4ecc10a6115f9ff74bf2b302f5f6ec88b
lakenoen <lakenoen@gmail.com>
2026-05-23 19:22:06 +0500

    fix db context_

---
+++ DbManager/Settings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbManager;
+
+public class Settings
+{
+    public string ConnectionDB {get;set;} = "Host=localhost;Username=admin;Password=pass;Database=PetrolTracker";
+    public bool UpdateDB {get;set;} = true;
+}
